feb70b7327cdffdc1ffc87e7ebb7a66f64f67a4f
hagajoel <joel.andriamasivelo@gmail.com>
2023-03-28 11:59:45 +0200

    sprint 1

---
+++ src/etu1930/framework/servlet/FrontServlet.java
@@ -0,0 +1,46 @@
+package etu1930.framework.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class FrontServlet extends HttpServlet{
+    protected void processRequest(HttpServletRequest req, HttpServletResponse rep) {
+        rep.setContentType("text/plain");
+        try (PrintWriter out = rep.getWriter()){
+            out.println(req.getRequestURI());
+            out.println(getUrl(req));
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        processRequest(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        processRequest(req, resp);
+    }
+
+    public String getUrl(HttpServletRequest req) {
+        String result;
+        String contextPath = req.getContextPath();
+        String url = req.getRequestURI();
+        result = url.split(contextPath)[1];
+        String query = req.getQueryString();
+        if (query != null) {
+            result = result.concat("?" + query);
+        }
+        return result;
+    }
+}
